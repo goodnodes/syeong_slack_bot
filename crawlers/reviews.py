12e7c1c1d09e99b6bccbb312cd7fa391c0ea3b56
Jacob <100544538+jongseokleedev@users.noreply.github.com>
2024-09-06 15:05:00 +0900

    for test

--- crawlers/reviews.py
+++ crawlers/reviews.py
@@ -87,17 +87,16 @@ def check_for_new_reviews():
     new_reviews = []
 
     for review in reviews:
-        print(f"{review['id']['label']}")
         if last_review_id and review['id']['label'] == last_review_id:
             break
         new_reviews.append(review)
     if new_reviews:
         new_reviews.reverse()
-        for review in new_reviews:
-            try:
-                response = client.chat_postMessage(channel=SLACK_USER_VOICE_CHANNEL_ID,text=format_review(review))
-            except SlackApiError as e:
-                print(f"Error posting slack message: {e}")
+        # for review in new_reviews:
+        #     try:
+        #         response = client.chat_postMessage(channel=SLACK_USER_VOICE_CHANNEL_ID,text=format_review(review))
+        #     except SlackApiError as e:
+        #         print(f"Error posting slack message: {e}")
         save_last_review_id(new_reviews[-1]['id']['label'])
 
 
