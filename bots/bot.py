7462a16631f83648aafe0a9cf7db9a3287358da8
Jacob <100544538+jongseokleedev@users.noreply.github.com>
2025-01-12 13:56:53 +0900

    👔 (feat): bot msg

--- bots/bot.py
+++ bots/bot.py
@@ -9,9 +9,9 @@ from slack_sdk.errors import SlackApiError
 load_dotenv()
 SLACK_ALARMY_OAUTH_TOKEN = os.environ['SLACK_ALARMY_OAUTH_TOKEN']
 SLACK_NOTIFICATIONS_CHANNEL_ID = os.environ['SLACK_NOTIFICATIONS_CHANNEL_ID']
+SLACK_RANDOM_CHANNEL_ID = os.environ['SLACK_RANDOM_CHANNEL_ID']
 SLACK_DEV_CHANNEL_ID = os.environ['SLACK_DEV_CHANNEL_ID']
 BOT_MSG = os.environ['BOT_MSG']
-UP_AND_DOWN_COMMENTS_FILE_PATH = "crawlers/comments/up_and_down_comments.json"
 #########################################################################
 client = WebClient(token=SLACK_ALARMY_OAUTH_TOKEN)
 
@@ -25,15 +25,9 @@ client = WebClient(token=SLACK_ALARMY_OAUTH_TOKEN)
 
 def post_msg():
     msg = BOT_MSG
-    # print(msg)
     try:
-        response = client.chat_postMessage(channel=SLACK_NOTIFICATIONS_CHANNEL_ID,
+        response = client.chat_postMessage(channel=SLACK_RANDOM_CHANNEL_ID,
                                            text=msg)
-        # response = client.chat_postMessage(channel="C07FQEVF3V2", thread_ts="", text="")
-        # client.conversations_list()
-        # response = client.conversations_history(channel="C07FQEVF3V2")
-        # save_id(response)
-        # print(response)
     except SlackApiError as e:
         print(f"Error posting slack message: {e}")
 
